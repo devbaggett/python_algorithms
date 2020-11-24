550105df5d1a91cef47bfd319fc8c9ac0de2180d
devbaggett <devabaggett@gmail.com>
2020-11-23 17:04:56 -0800

    Added second string_compression function.

--- string_compression.py
+++ string_compression.py
@@ -10,10 +10,36 @@ from tests.tests import TestCompress
 from collections import Counter
 
 
+# not taking in account order
 def string_compression(string):
     char_counts = Counter(string)
     return ''.join('{}{}'.format(key, value) for key, value in char_counts.items())
 
 
+# taking in account order
+def string_compression2(string):
+    new_str = ""
+
+    length = len(string)
+    if length == 0:
+        return ""
+    if length == 1:
+        return string + "1"
+
+    count = 1
+    i = 1
+    while i < length:
+        if string[i] == string[i - 1]:
+            count += 1
+        else:
+            new_str += string[i - 1] + str(count)
+            count = 1
+        i += 1
+
+    new_str += string[i - 1] + str(count)
+
+    return new_str
+
+
 t = TestCompress()
-t.test(string_compression)
+t.test(string_compression2)
