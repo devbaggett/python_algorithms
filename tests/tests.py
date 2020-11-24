ec6f141b092d78f8324d5d11ff39e66330099413
devbaggett <devabaggett@gmail.com>
2020-11-23 18:23:34 -0800

    Added and updated __init__.py and added additional tests.

--- tests/tests.py
+++ tests/tests.py
@@ -11,3 +11,57 @@ class AnagramTest(object):
         assert_equal(sol('aabbcc', 'aabbc'), False)
         assert_equal(sol('123', '1 2'), False)
         print("ALL TEST CASES PASSED")
+
+
+class TestPair(object):
+
+    def test(self, sol):
+        assert_equal(sol([1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1], 10), 6)
+        assert_equal(sol([1, 2, 3, 1], 3), 1)
+        assert_equal(sol([1, 3, 2, 2], 4), 2)
+        print('ALL TEST CASES PASSED')
+
+
+class TestFinder(object):
+
+    def test(self, sol):
+        assert_equal(sol([5, 5, 7, 7], [5, 7, 7]), 5)
+        assert_equal(sol([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6]), 5)
+        assert_equal(sol([9, 8, 7, 6, 5, 4, 3, 2, 1], [9, 8, 7, 5, 4, 3, 2, 1]), 6)
+        print('ALL TEST CASES PASSED')
+
+
+class LargeContTest(object):
+    def test(self, sol):
+        assert_equal(sol([1, 2, -1, 3, 4, -1]), 9)
+        assert_equal(sol([1, 2, -1, 3, 4, 10, 10, -10, -1]), 29)
+        assert_equal(sol([-1, 1]), 1)
+        print('ALL TEST CASES PASSED')
+
+
+class ReversalTest(object):
+
+    def test(self, sol):
+        assert_equal(sol('    space before'), 'before space')
+        assert_equal(sol('space after     '), 'after space')
+        assert_equal(sol('   Hello John    how are you   '), 'you are how John Hello')
+        assert_equal(sol('1'), '1')
+        print("ALL TEST CASES PASSED")
+
+
+class TestCompress(object):
+
+    def test(self, sol):
+        assert_equal(sol(''), '')
+        assert_equal(sol('AABBCC'), 'A2B2C2')
+        assert_equal(sol('AAABCCDDDDD'), 'A3B1C2D5')
+        print('ALL TEST CASES PASSED')
+
+
+class TestUnique(object):
+
+    def test(self, sol):
+        assert_equal(sol(''), True)
+        assert_equal(sol('goo'), False)
+        assert_equal(sol('abcdefg'), True)
+        print('ALL TEST CASES PASSED')
